0f190f31e375f7de08e3a5026586b8f451c1269b
Korka13 <korka1313@gmail.com>
2019-07-23 06:34:39 +0000

    Updates

---
+++ precache-manifest.84ac51d9228472c7036bd593516345af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462fb01a69348d3c25090567dbd075de",
+    "url": "/qoqtails/index.html"
+  },
+  {
+    "revision": "6e906760d72218536c95",
+    "url": "/qoqtails/static/css/main.935be14c.chunk.css"
+  },
+  {
+    "revision": "f5e31853dabb6efa0419",
+    "url": "/qoqtails/static/js/2.bb2344ff.chunk.js"
+  },
+  {
+    "revision": "6e906760d72218536c95",
+    "url": "/qoqtails/static/js/main.55d2b273.chunk.js"
+  },
+  {
+    "revision": "200b42effd5536c53c11",
+    "url": "/qoqtails/static/js/runtime~main.ef29d4bf.js"
+  },
+  {
+    "revision": "2fb3d4c620de5a70173aeaaf4f9062d9",
+    "url": "/qoqtails/static/media/github.2fb3d4c6.svg"
+  }
+]);
